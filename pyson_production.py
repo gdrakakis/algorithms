b9b08421388764f8fda8ced1339fdb2d0ba9cc9b
gdrakakis <gdrakakis356@gmail.com>
2016-03-30 15:09:21 +0300

    len = 0

--- pyson_production.py
+++ pyson_production.py
@@ -1736,7 +1736,7 @@ class StreamConsumingMiddleware(object):
 
     def __call__(self, environ, start_response):
         stream = LimitedStream(environ['wsgi.input'],
-                               int(environ['CONTENT_LENGTH'] or 0))
+                               int(0))
         environ['wsgi.input'] = stream
         app_iter = self.app(environ, start_response)
         try:
